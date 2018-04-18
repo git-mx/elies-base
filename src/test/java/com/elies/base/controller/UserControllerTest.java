e64530a4561346593cf81e6b71505b9de49da04a
gix-mx <464293746@qq.com>
2018-04-18 14:07:37 +0800

    单元测试修改

--- src/test/java/com/elies/base/controller/UserControllerTest.java
+++ src/test/java/com/elies/base/controller/UserControllerTest.java
@@ -7,13 +7,15 @@ package com.elies.base.controller;
  * @since 2018/4/4
  */
 
+import com.alibaba.fastjson.JSONObject;
 import com.elies.base.domain.UserInsert;
 import com.elies.base.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 public class UserControllerTest extends BaseControllerTestCase {
     @Autowired
@@ -29,10 +31,13 @@ public class UserControllerTest extends BaseControllerTestCase {
         userInsert.setUserAge(18);
         userInsert.setUserName("DFSDFDS");
         userInsert.setUserSex(1);
+        String requestJson = JSONObject.toJSONString(userInsert);
         for(int i=0; i<50; i++){
-            mockMvc.perform(get("/user/insertUser")
-                    .param("userInsert", ""))
-                    .andExpect(status().isOk());
+            mockMvc.perform( post("/user/insertUser")
+                    .contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andReturn();
         }
     }
 
